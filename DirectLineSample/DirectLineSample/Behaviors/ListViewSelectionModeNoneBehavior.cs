c50312037206b45e90abba939123c1a28c464785
Kazuki Ota <k_ota28@hotmail.com>
2017-06-17 17:32:49 +0900

    ListViewを選択できなくした

---
+++ DirectLineSample/DirectLineSample/Behaviors/ListViewSelectionModeNoneBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DirectLineSample.Behaviors
+{
+    public class ListViewSelectionModeNoneBehavior : Behavior<ListView>
+    {
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.ItemSelected += this.Bindable_ItemSelected;
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.ItemSelected -= this.Bindable_ItemSelected;
+        }
+
+        private void Bindable_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
