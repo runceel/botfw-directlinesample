f799ed07241c5f309dd80083d0380730eca6870d
Kazuki Ota <k_ota28@hotmail.com>
2017-06-17 17:41:49 +0900

    ChannelAccountのNameがないときはIdを表示するようにした

---
+++ DirectLineSample/DirectLineSample/Converters/ChannelAccountToTextConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Bot.Connector.DirectLine;
+using Xamarin.Forms;
+
+namespace DirectLineSample.Converters
+{
+    public class ChannelAccountToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var channelAccount = (ChannelAccount)value;
+            return string.IsNullOrEmpty(channelAccount.Name) ? channelAccount.Id : channelAccount.Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
